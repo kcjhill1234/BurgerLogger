47f56f5f45065fa64abc4761f23516c654f64390
ktotheH <kcjhill1234@msn.com>
2020-08-17 16:59:40 -0600

    server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const mysql2 = require("mysql2");
 
-var app = express();
-
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
+app.use(express.static("public"));
 
 const connection = mysql2.createConnection({
     host: "localhost",
@@ -14,8 +15,7 @@ const connection = mysql2.createConnection({
     database: "burgers_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
+app.listen(PORT, function () {
+    console.log("Server is listening on: http://localhost: " + PORT);
 });
+
