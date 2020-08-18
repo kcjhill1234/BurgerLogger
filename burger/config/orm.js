62c96f505e3f3537e56f4835d1b352f9cc62e69c
ktotheH <kcjhill1234@msn.com>
2020-08-17 19:31:30 -0600

    :)

---
+++ burger/config/orm.js
@@ -0,0 +1,20 @@
+const connection = require("./connection")
+
+
+const orm = {
+
+selectAll: function () {
+
+}
+
+insertOne: function () {
+
+}
+
+updateOne: function () {
+
+}
+
+}
+
+module.exports = orm;
