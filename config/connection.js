ae179f4d649131d82855517e3821650fedc06f5b
ktotheH <kcjhill1234@msn.com>
2020-08-18 21:42:25 -0600

    db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const  mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+let connection = null 
+if (process.env.NODE_ENV === "production") {
+    connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
+
 
 connection.connect(function (err) {
     if (err) {
